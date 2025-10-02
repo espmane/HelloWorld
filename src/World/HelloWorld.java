0c9dbbd11e56ceabfa47992a5c827cf2f8fb1e27
Esp <duckerlicious@duck.com>
2025-10-02 23:33:34 +0200

    printer anderledes nu

--- src/World/HelloWorld.java
+++ src/World/HelloWorld.java
@@ -1,7 +1,26 @@
 package World;
 
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Hvad er dit fornavn?: ");
+        String name = sc.nextLine();
+
+        StringBuilder sb = new StringBuilder("World");
+
+        while (!sb.isEmpty()) {
+            System.out.println("Hello " + sb);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+
+        System.out.println("Hello ");
+
+        for (int i = 0; i < name.length(); i++) {
+            sb.append(name.charAt(i));
+            System.out.println("Hello " + sb);
+        }
     }
 }
